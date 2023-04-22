9995691eb1b60cd1a2b915207cdd14b2078e345a
PavelMezko <pavelprimk@gmail.com>
2023-04-23 01:50:17 +1000

    intial commit

---
+++ Assets/З†§†≠®п/З†§†з† 4/Level4.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Level4 : MonoBehaviour
+{
+    public bool UseGravity = false;
+    private float _speed = 5;
+
+    private void Update()
+    {
+        
+        var step = Time.deltaTime * _speed;
+        var transformPosition = transform.position;
+        transformPosition.x += step;
+
+        transform.position = transformPosition; 
+    }
+}
